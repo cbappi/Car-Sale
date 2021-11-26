d5f0878aa0f91316d7c345a7407f8cd73413c375
cbappi <abdulwahedrr@gmail.com>
2021-11-26 07:30:30 +0600

    first commit for car

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+
+  
+  
+        apiKey: "AIzaSyDHkc5KUGM6fNhN1j2ZfuwXif2bMm9qfHI",
+        authDomain: "assignmen-12.firebaseapp.com",
+        projectId: "assignmen-12",
+        storageBucket: "assignmen-12.appspot.com",
+        messagingSenderId: "858382421152",
+        appId: "1:858382421152:web:40fc652126a3a33d43cdc5"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+};
+export default firebaseConfig;
